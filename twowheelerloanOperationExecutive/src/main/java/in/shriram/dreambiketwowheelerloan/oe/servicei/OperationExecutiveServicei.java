4b3085637e6511b3aef4648ab3d26b3b69da76b3
Yogesh_Yeole <pranjalijamale@gmail.com>
2025-02-23 14:48:59 +0530

    Added Email Sender

--- twowheelerloanOperationExecutive/src/main/java/in/shriram/dreambiketwowheelerloan/oe/servicei/OperationExecutiveServicei.java
+++ twowheelerloanOperationExecutive/src/main/java/in/shriram/dreambiketwowheelerloan/oe/servicei/OperationExecutiveServicei.java
@@ -1,9 +1,20 @@
 package in.shriram.dreambiketwowheelerloan.oe.servicei;
 
+import java.util.List;
+
+import in.shriram.dreambiketwowheelerloan.oe.model.EmailSender;
 import in.shriram.dreambiketwowheelerloan.oe.model.Enquiry;
 
 public interface OperationExecutiveServicei {
 
 	public Enquiry updateEnquiryStatus(int cibilId, String status);
+
+	public Enquiry getEnquiryEmail(String to);
+
+	public EmailSender sendEmail(EmailSender e, int customerId);
+
+	
+
+	
 
 }
--- twowheelerloanOperationExecutive/src/main/java/in/shriram/dreambiketwowheelerloan/oe/serviceimpl/OperationExecutiveServiceImpl.java
+++ twowheelerloanOperationExecutive/src/main/java/in/shriram/dreambiketwowheelerloan/oe/serviceimpl/OperationExecutiveServiceImpl.java
@@ -1,10 +1,18 @@
 package in.shriram.dreambiketwowheelerloan.oe.serviceimpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.discovery.converters.Auto;
+
+import in.shriram.dreambiketwowheelerloan.oe.model.Cibil;
+import in.shriram.dreambiketwowheelerloan.oe.model.EmailSender;
 import in.shriram.dreambiketwowheelerloan.oe.model.Enquiry;
 import in.shriram.dreambiketwowheelerloan.oe.repository.OperationExecutiveCibilRepo;
 import in.shriram.dreambiketwowheelerloan.oe.repository.OperationExecutiveEnquiryRepo;
@@ -20,7 +28,14 @@ public class OperationExecutiveServiceImpl implements OperationExecutiveServicei
 	OperationExecutiveEnquiryRepo oerEnq;
 
 	@Autowired
+	OperationExecutiveEnquiryRepo oers;
+	
+	
+	@Autowired
 	RestTemplate rt;
+	
+	@Autowired
+	JavaMailSender sender;
 
 	@Override
 	public Enquiry updateEnquiryStatus(int cibilId, String status) {
@@ -34,5 +49,31 @@ public class OperationExecutiveServiceImpl implements OperationExecutiveServicei
 		
 		return eo;
 	}
+
+	@Override
+	public Enquiry getEnquiryEmail(String to) {
+		Enquiry ers= oerEnq.findByEmail(to);
+		return ers;
+	}
+
+	@Override
+	public EmailSender sendEmail(EmailSender e, int customerId) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		
+		Enquiry enq=rt.getForObject("http://localhost:7777/enq/enquiry/"+customerId, Enquiry.class);
+		e.setMessage("Customer with " + enq.getCustomerId()+" has a sucessfully done "
+		+enq.getCb().getCibilRemark() + "with enquiry and your with the status "+enq.getCb().getStatus());
+
+		message.setTo(e.getToEmail());
+		message.setFrom(e.getFromEmail());
+		message.setSubject(e.getSubject());
+		message.setText(e.getMessage());
+		sender.send(message);
+	
+		return e;
+		
+	}
+
+
 }
 
