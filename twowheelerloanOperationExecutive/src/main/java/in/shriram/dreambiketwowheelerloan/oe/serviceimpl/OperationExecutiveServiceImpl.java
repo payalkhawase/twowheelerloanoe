ceda120f211f70826d59e72c5979453ce9666e23
payalkhawase <payal.khawase14@gmail.com>
2025-03-05 17:15:53 +0530

    add

--- twowheelerloanOperationExecutive/src/main/java/in/shriram/dreambiketwowheelerloan/oe/serviceimpl/OperationExecutiveServiceImpl.java
+++ twowheelerloanOperationExecutive/src/main/java/in/shriram/dreambiketwowheelerloan/oe/serviceimpl/OperationExecutiveServiceImpl.java
@@ -107,7 +107,7 @@ public class OperationExecutiveServiceImpl implements OperationExecutiveServicei
 	@Override
 	public Customer getcustomer(int customerId, String loanStatus) {
 		
-		Customer cust=rt.getForObject("http://localhost:7777/apploan/getCustomer/"+customerId, Customer.class);
+		Customer cust=rt.getForObject("http://localhost:7777/apploan/getaCustomer/"+customerId, Customer.class);
 	
 		cust.setLoanStatus(loanStatus);
 		//cust.setCibil();
@@ -118,7 +118,7 @@ public class OperationExecutiveServiceImpl implements OperationExecutiveServicei
 	@Override
 	public CustomerVerification addVerifictiondetails(CustomerVerification cu, int customerId) {
 	
-     Customer cust=rt.getForObject("http://localhost:7777/apploan/getCustomer/"+customerId, Customer.class);
+     Customer cust=rt.getForObject("http://localhost:7777/apploan/getaCustomer/"+customerId, Customer.class);
 		
 		CustomerVerification cvo=ov.save(cu);
 		//cust.setLoanStatus(loanStatus);
